90e102cbeb2c445dbab3e710fe673a5f01e5a84f
tyborg01 <tyborg01>
2018-09-27 23:10:11 +0300

    ex3 changed

--- ex3.py
+++ ex3.py
@@ -1,9 +1,8 @@
-import turtle
 import math
 turtle.shape('turtle')
 a = 50
 for i in range(360):
     t = i * math.pi/180
-    x = 2*a*math.cos(t) - a*math.cos(2*t)
+    x = 2*a*math.cos(t) - a*math.cos(2*t) - a
     y = 2*a*math.sin(t) - a*math.sin(2*t)
     turtle.goto(x, y)
